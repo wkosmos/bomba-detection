534b9cd871b77ea273d3ba829ed161b470b257ef
wkosmos <wkosmos@posteo.net>
2020-09-13 21:55:32 -0700

    add todos

---
+++ src/data_prep_functions.py
@@ -0,0 +1,13 @@
+import librosa as lb
+import pandas as pd
+import numpy as np
+
+
+
+def detect_beats(y, sample_rate=44100, start_bpm=90):
+
+    lb.beat.beat_track(y, )
+
+
+def make_spectrogram(y, ):
+    pass
